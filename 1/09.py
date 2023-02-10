0bc52e86c12055f9ec051c5868f26e57c1698df8
Mihir Chakma <mihirchakmain@gmail.com>
2023-02-10 22:48:58 +0530

    updated

--- 1/09.py
+++ 1/09.py
@@ -1,10 +1,8 @@
+print('"Bang!" with a beep')
+
 print("Bang!", '\a')
 
 print("Bang! '\a' ")
 
-
 print("\"Bang!\" with a beep")
-
-# you can use both of them, both are same 
-print('"Bang!" with a beep')
 
